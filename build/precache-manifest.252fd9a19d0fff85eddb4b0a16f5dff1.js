526c80211b52665b902f9b7511ed2e4c99c8a5fe
tehmansardar <tehmansardar@gmail.com>
2020-04-02 02:35:10 +0500

    add production build

---
+++ build/precache-manifest.252fd9a19d0fff85eddb4b0a16f5dff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c861f80362bda9fa674d1f1d4ca5c42a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee2aae54522a35676cab",
+    "url": "/static/css/main.1a2eab51.chunk.css"
+  },
+  {
+    "revision": "454360d1096cfb53b388",
+    "url": "/static/js/2.dc663850.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.dc663850.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee2aae54522a35676cab",
+    "url": "/static/js/main.76e0bc24.chunk.js"
+  },
+  {
+    "revision": "7aaf91a552c540c62644",
+    "url": "/static/js/runtime-main.a15bdacd.js"
+  }
+]);
